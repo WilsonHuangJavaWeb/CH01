563cf23849c64a7811660d2767fe6a51994c4057
WILSON-HOME\WilsonHuang <hjn201@gmail.com>
2017-02-05 01:37:27 +0800

    只是練習

---
+++ src/chapter1/diary/bean/Diary.java
@@ -0,0 +1,55 @@
+package chapter1.diary.bean;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Created by WilsonHuang on 2017/2/5.
+ */
+public class Diary {
+    private String title;
+    private String content;
+    private GregorianCalendar time;
+    private String authorName;
+
+    public Diary() {
+    }
+
+    public Diary(String title, String content, GregorianCalendar time, String authorName) {
+        this.title = title;
+        this.content = content;
+        this.time = time;
+        this.authorName = authorName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public GregorianCalendar getTime() {
+        return time;
+    }
+
+    public void setTime(GregorianCalendar time) {
+        this.time = time;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
